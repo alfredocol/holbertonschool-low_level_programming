710ea1eb8fac871b8cf890fae78d3fea29a61d11
Alfredo Colon <710@holbertonschool.com>
2019-02-07 11:46:09 -0800

    solution using a macro define statement

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#define IS(n) n == 0 ? "is zero" : n < 5 ? "is greater than 5" : "is less than six"
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d is %d and %s\n", n, n % 10, IS(n % 10));
+	return (0);
+}
