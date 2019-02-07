cc449ee9fae8247eda214f0addc3eb12e51014ed
Alfredo Colon <710@holbertonschool.com>
2019-02-07 21:13:39 +0000

    fixed betty errors

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-#define PRINT_SIGN(n) n<0 ? printf("%d is negative\n", n) : n==0 ? printf("%d is zero\n", n) : printf("%d is positive\n", n)
+#define PRINT_SIGN(n) (n < 0 ? printf("%d is negative\n", n) : n == 0 ? \
+printf("%d is zero\n", n) : printf("%d is positive\n", n))
 
 int main(void)
 {
