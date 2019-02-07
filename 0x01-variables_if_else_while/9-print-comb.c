0305cbd6c290e0d97e294759dd785fcc20adff8e
Alfredo Colon <710@holbertonschool.com>
2019-02-07 15:09:08 -0800

    more fleshed out answer

--- 0x01-variables_if_else_while/9-print-comb.c
+++ 0x01-variables_if_else_while/9-print-comb.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 
+/**
+ * main - returns 0
+ * @void: void
+ *
+ * Description: outputs 0..9 with a comma and space for < 9
+ * Return: 0;
+ */
 int	main(void)
 {
-	char	*str = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9";
-	
-	while (*str)
-		putchar(*str++);
+	char	c;
+
+	c = '0';
+	while (c <= '9')
+	{
+		putchar(c);
+		if (c < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
 	putchar('\n');
 	return (0);
 }
