379d9cbb304e897ea3c43ae5595066c30dc1966a
Alfredo Colon <710@holbertonschool.com>
2019-02-07 16:38:20 -0800

    10 & 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,29 +2,31 @@
 
 /**
  * main - returns 0
- * @void: void
+ * @void - void
  *
- * Description: prints numbers from 00..99
+ * Description: prints all possible different combinations
+ * of two digits with no repititions
  * Return: 0
  */
+
 int	main(void)
 {
-	int	i,j;
+	int	i, j;
 	
 	i = -1;
 	while (++i < 10)
 	{
-		j = -1;
-		while (++j < 10)
+		j = i + 1;
+		while (j < 10)
 		{
 			putchar(i + 48);
 			putchar(j + 48);
-			if (i == 9 && j == 9)
+			if (i == 8 && j == 9)
 				break;
 			putchar(',');
 			putchar(' ');
+			j++;
 		}
-		
 	}
 	putchar('\n');
 	return (0);
