cc20b75b4c7408c5be66d155ddd596dcf8fb2902
Alfredo Colon <710@holbertonschool.com>
2019-02-12 00:22:50 +0000

    solution

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10x
+ * @void: void
+ *
+ * Description: prints alphabet 10x
+ * Return: ;
+ */
+void	print_alphabet_10x(void)
+{
+	char	i, letter;
+
+	letter = '`';
+	for (i = 0; i < 10; i++)
+	{	
+		for (letter = 'a'; letter < '{'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
+}
