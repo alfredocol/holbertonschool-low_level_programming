d00c1619a8504a0df07e18b3a64bd87eb3c864c5
Alfredo Colon <710@holbertonschool.com>
2019-02-13 00:13:54 +0000

    solution

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_sign - prints sign of n
+ * @n: integer
+ *
+ * Description: prints sign of a number
+ * Return: 1 if positive, 0 if 0 and -1 if negative
+ */
+int	print_sign(int n)
+{
+	if (!n)
+	{
+		_putchar('0');
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+}
