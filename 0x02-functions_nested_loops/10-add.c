503648e508238819a436f4986a23d104225146a2
Alfredo Colon <710@holbertonschool.com>
2019-02-13 01:47:25 +0000

    solution

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * add - adds two integers and returns the result.
+ * @n: first number
+ * @n1: second number
+ *
+ * Description: adds two integers and returns the result
+ * Return: combined value of passed in intergers
+ */
+int	add(int n, int n1)
+{
+	return (n + n1);
+}
