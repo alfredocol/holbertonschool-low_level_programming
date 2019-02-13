ec0213c5a0ddbb0e8edfde886d80e1052a4f1f42
Alfredo Colon <710@holbertonschool.com>
2019-02-13 01:56:08 +0000

    solution

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints natural numbers from n..98
+ * @n: starting int
+ *
+ * Description: loops from n..98, prints number
+ * Return: ;
+ */
+void	print_to_98(int n)
+{
+	for (; n < 99; n++)
+	{
+		printf("%d", n);
+		if (n == 98)
+			break;
+		printf(", ");
+	}
+	printf("\n");
+}
