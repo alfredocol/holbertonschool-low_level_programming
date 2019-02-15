166273b1824ccf79a13958efe3e037b9875b2995
Alfredo Colon <710@holbertonschool.com>
2019-02-15 04:27:26 +0000

    10

---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - prints triangle followed by a new line
+ * @size: size of triangle
+ *
+ * Description: prints triangle followed by a new line
+ * Return: ;
+ */
+void	print_triangle(int size)
+{
+	int	line, spaces, hashes;
+	/*if size > 0*/
+	if (size)
+	{
+		for (line = 0; line < size; line++)
+		{
+			spaces = size - line - 1;
+			hashes = line + 1;
+			while (spaces--)
+				_putchar(' ');
+			while (hashes--)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
