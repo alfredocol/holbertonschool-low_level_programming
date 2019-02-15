b2425894a9865a5490f83dd0967c20e2d7c24948
Alfredo Colon <710@holbertonschool.com>
2019-02-15 06:00:39 +0000

    fizzbuzz

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * fizz_buzz - prints 1..100 followed by new line.
+ * prints Fizz for multiples of 3
+ * prints Buzz for multiple of 5
+ * prints FizzBuzz for multiples of 3 and 5
+ * @void:
+ *
+ * Description: see fizz_buzz :)
+ * Return: ;
+ */
+void	fizz_buzz(void)
+{
+	char	i;
+
+	i = 0;
+	while (++i < 101)
+	{
+		if (!(i % 3) || !(i % 5))
+		{
+			if (!(i % 3))
+				printf("Fizz");
+			if (!(i % 5))
+				printf("Buzz");
+		}
+		else
+			printf("%d", i);
+		if (i == 100)
+			printf("\n");
+		else
+			printf(" ");
+	}
+}
