dc9aa00a99835aa4627dcc06ed0c5cb2d33d3fef
alfredocol <47125664+alfredocol@users.noreply.github.com>
2019-02-18 08:22:59 -0800

    Update 3-puts.c
    Solution

--- 0x04-pointers_arrays_strings/3-puts.c
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,12 @@
-
+/**
+ * _puts - prints a string, followed by a new line
+ * @*str: string
+ *
+ * Description: prints a string, followed by a new line
+ * Return: ;
+ */
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+}
