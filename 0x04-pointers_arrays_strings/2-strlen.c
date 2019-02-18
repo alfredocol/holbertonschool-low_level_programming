3cd7720f9b496207a0f708fca683944b6cf9241b
alfredocol <47125664+alfredocol@users.noreply.github.com>
2019-02-18 08:22:01 -0800

    Update 2-strlen.c
    Solution

--- 0x04-pointers_arrays_strings/2-strlen.c
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,16 @@
-
+/**
+ * _strlen - returns the length of a string.
+ * @*s: string
+ *
+ * Description: returns the length of a string.
+ * Return: len of string
+ */
+int _strlen(char *s)
+{
+	int		len = 0;
+	
+	while (s[len])
+		len++;
+	return (len);
+}
+/**
