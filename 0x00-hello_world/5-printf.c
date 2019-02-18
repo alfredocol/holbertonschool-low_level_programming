c7a4dc1e6ef47d37af6043bc999e8fdce11aaaac
alfredocol <47125664+alfredocol@users.noreply.github.com>
2019-02-17 19:31:05 -0800

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - displays text
+ * @void: void
+ *
+ * Description: displays text
+ * Return: 0
+ */
 int	main(void)
 {
 	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
