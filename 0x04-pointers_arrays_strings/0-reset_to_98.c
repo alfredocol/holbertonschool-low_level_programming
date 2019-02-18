23f5d6c95e0ea2be830a9bdcb4ffca0ae1f7ddf8
alfredocol <47125664+alfredocol@users.noreply.github.com>
2019-02-18 08:19:14 -0800

    Update 0-reset_to_98.c
    Solution

--- 0x04-pointers_arrays_strings/0-reset_to_98.c
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -1,1 +1,11 @@
-
+/**
+ * reset_to_98 - takes i int pointer, updates vakue to 98
+ * @*n: int pointer
+ *
+ * Description: takes in int pointer and updates value to 98
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
