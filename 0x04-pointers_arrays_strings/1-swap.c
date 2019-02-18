f6d6d3650a76debb8dd612c64ff394029af74afb
alfredocol <47125664+alfredocol@users.noreply.github.com>
2019-02-18 08:21:25 -0800

    Update 1-swap.c
    Solution

--- 0x04-pointers_arrays_strings/1-swap.c
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,15 @@
-
+/**
+ * swap_int - swaps values of passed in int pointers
+ * @*a: int pointer
+ * @*b
+ *
+ * Description: swaps values of passed in int pointers
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int		temp = *a;
+	
+  *a = *b;
+	*b = temp;
+}
