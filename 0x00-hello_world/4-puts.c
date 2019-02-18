ffd5fed2f4b2bb694790d9821875d421a4a3d1af
alfredocol <47125664+alfredocol@users.noreply.github.com>
2019-02-17 19:34:18 -0800

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - diaplays text
+ * @void: void
+ *
+ * Description: displays text
+ * Return: 0
+ */
 int	main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
-}	
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
 
