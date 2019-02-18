936c6139aec65bc26e4f7ac574a18aeb55812f2b
alfredocol <47125664+alfredocol@users.noreply.github.com>
2019-02-17 20:55:54 -0800

    Update 5-more_numbers.c

--- 0x03-more_functions_nested_loops/5-more_numbers.c
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -10,10 +10,10 @@
  */
 void	more_numbers(void)
 {
-	char	i, j;
+	char	i, j, k;
 	char	n[] = "01234567891011121314\n";
 
 	for (i = 0; i < 10; i++)
 		for (j = 0; j < 21; j++)
-			_putchar((int)n[j]);
+			_putchar(k = n[j]);
 }
