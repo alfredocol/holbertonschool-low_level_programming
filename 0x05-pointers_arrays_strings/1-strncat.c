7732080e474f65abca7ccd359a6f80289b572ce4
alfredocol <47125664+alfredocol@users.noreply.github.com>
2019-02-21 22:04:09 -0800

    Update 1-strncat.c

--- 0x05-pointers_arrays_strings/1-strncat.c
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,15 @@
+#include "holberton.h"
 
+/**
+ * _strcmp - concat n char of src to dest
+ * @dest: string
+ * @src: string
+ * @n: number of elements fo copy
+ *
+ * return: concatenated str
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	return (dest);
+
+}
