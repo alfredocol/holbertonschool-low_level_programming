c5dfb4706d96e4c47d6ca15deaaba5bcd59849b7
alfredocol <47125664+alfredocol@users.noreply.github.com>
2019-02-21 21:14:03 -0800

    Create 100-print_number.c
    Solution

---
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,22 @@
+/**
+ * print_number - prints number
+ * @n: integer to print
+ *
+ * return: void
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		print_number(-(n / 10));
+		print_number(-(n % 10));
+	}
+	if (n > 9)
+	{
+		print_number(n / 10);
+		print_number(n % 10);
+	}
+	if (n < 10)
+		_putchar(n + '0');
+}
