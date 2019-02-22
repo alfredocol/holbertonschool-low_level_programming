dccdd9dc8c1aae2e54f2b8acfff0d02d21867ac3
alfredocol <47125664+alfredocol@users.noreply.github.com>
2019-02-21 21:55:33 -0800

    Update 4-rev_array.c

--- 0x05-pointers_arrays_strings/4-rev_array.c
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -1,1 +1,12 @@
+#include "holberton.h"
 
+/**
+ * reverse_array - reverses order of elements
+ * @s: array of ints
+ * @n: number of elements
+ *
+ * return: ;
+ */
+void reverse_array(int *a, int n)
+{
+}
