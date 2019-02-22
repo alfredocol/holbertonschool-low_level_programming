c03a297568ffc6448c321dfeaca442f84804083e
alfredocol <47125664+alfredocol@users.noreply.github.com>
2019-02-21 22:00:26 -0800

    Update 2-strncpy.c

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,15 @@
+#include "holberton.h"
 
+/**
+ * _strcmp - compares two strings
+ * @dest: string
+ * @src: string
+ * @n: number of elements fo copy
+ *
+ * return: diff
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (dest);
+
+}
