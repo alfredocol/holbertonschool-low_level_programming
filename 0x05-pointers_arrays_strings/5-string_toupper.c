154921c001e17f0e1e47c39f2e2f594f76842b76
alfredocol <47125664+alfredocol@users.noreply.github.com>
2019-02-21 21:52:16 -0800

    Update 5-string_toupper.c

--- 0x05-pointers_arrays_strings/5-string_toupper.c
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,12 @@
+#include "holberton.h"
 
+/**
+ * string_toupper - changes lowercase up uppercase
+ * @s: string
+ *
+ * return: uppercased string
+ */
+char *string_toupper(char *)
+{
+	return (s);
+}
