ff2b63f850e8c3d141af7749d0771c12e7079dda
alfredocol <47125664+alfredocol@users.noreply.github.com>
2019-02-21 22:07:07 -0800

    Update 0-strcat.c

--- 0x05-pointers_arrays_strings/0-strcat.c
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,14 @@
+#include "holberton.h"
 
+/**
+ * _strcat - concats src to dest
+ * @dest: string
+ * @src: string
+ *
+ * return: concatenated str
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (dest);
+
+}
