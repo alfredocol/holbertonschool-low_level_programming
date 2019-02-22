035b7f80a22cbe6880aff6c38b5dc7642771b036
alfredocol <47125664+alfredocol@users.noreply.github.com>
2019-02-21 21:58:30 -0800

    Update 3-strcmp.c

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -1,1 +1,15 @@
+#include "holberton.h"
 
+/**
+ * _strcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * return: diff
+ */
+int _strcmp(char *s1, char *s2);
+{
+	int diff = 0;
+
+	return (diff);
+}
