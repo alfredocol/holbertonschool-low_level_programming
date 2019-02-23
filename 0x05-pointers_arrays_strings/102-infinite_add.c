b6e64b92393cffc2f6b28feb36314941ff626529
Alfredo Colon <710@holbertonschool.com>
2019-02-23 04:25:13 +0000

    its working

--- 0x05-pointers_arrays_strings/102-infinite_add.c
+++ 0x05-pointers_arrays_strings/102-infinite_add.c
@@ -16,64 +16,112 @@ int	_strlen(char *s)
 		len++;
 	return (len);
 }
+/**
+ * _strlen - returns the length of a string.
+ * @s: string
+ *
+ * Description: returns the length of a string.
+ * Return: len of string
+ */
+char	*_bzero(char r[], int size)
+{
+	char	*p = r;
+	
+	while (size--)
+		*p++ = '0';
+	return (r);
+}
+/**
+ * _strlen - returns the length of a string.
+ * @s: string
+ *
+ * Description: returns the length of a string.
+ * Return: len of string
+ */
+char	*add(char r[], int n, int i)
+{
+	int	sum;
+	
+	sum = r[i] - '0' + n;
+	r[i] = sum % 10 + '0';
+	if (sum > 9)
+	{
+		if (!i)
+			return (0);
+		r = add(r, sum / 10, i - 1);
+	}
+	return (r);
+} 
+/**
+ * _strlen - returns the length of a string.
+ * @s: string
+ *
+ * Description: returns the length of a string.
+ * Return: len of string
+ */
+char	*benotzero(char *r, int offset, int len)
+{
+	int	i = 0;
+	int	end = len - offset;
+	printf("%d\n", offset);
+	while (i < end)
+	{
+		r[i] = r[i + offset];
+		i++;
+	}
 
+	int	j = -1;
+	while (++j < 6)
+		printf("%c", r[j]);
+	printf("\n");
+	return (r);
+}
+/**
+ * _strlen - returns the length of a string.
+ * @s: string
+ *
+ * Description: returns the length of a string.
+ * Return: len of string
+ */
 char *infinite_add(char *n, char *m, char *r, int size)
 {
-	int	nlen = _strlen(n) - 1;
-	int	mlen = _strlen(m) - 1;
-	int sum;
-	int carryover;
-	int i;
-  
-	/*check if size is large enough to hold sum*/
-	if (mlen > size || nlen > size)
+	int	nlen = _strlen(n);
+	int	mlen = _strlen(m);
+	int	sum;
+  	int	len = size;
+
+	r = _bzero(r, size);		
+	if (mlen - 1 > size || nlen - 1 > size)
 		return (0);
 	while (size--)
 	{
-		sum = 0;
-		carryover = 0;
-		//get new sum
-		if (m[mlen])
-			sum += m[mlen] - '0';
-		if (n[nlen])
-			sum += n[nlen] - '0';
-		//update subscript
-		r[size] = sum % 10 + '0';
-		//check for carryover
-		if (sum > 9)
-		{
-			if (!size)
-				return (0);
-			if (r[size - 1] > '0' && r[size - 1] < '9')
-			{
-				i = 0;
-				while (r[size - ++i] && (size - i))
-				{
-					sum = sum / 10 + (r[size - i] - '0');
-					r[size - i] = sum % 10 + '0';
-					if (sum < 10)
-						break;
-				}
-			}
-		}
-		r[size - 1] = sum / 10 + '0';
 		mlen--;
 		nlen--;
-		if (!mlen && !nlen)
+		sum = 0;
+		if (mlen > -1)
+			sum += m[mlen] - '0';
+		if (nlen > -1)
+			sum += n[nlen] - '0';
+		if (!(r = add(r, sum, size)))
+			return (0);
+		if (mlen <= 0 && nlen <= 0)
 			break ;
 	}
-	
+	r = benotzero(r, size, len);
 	return (r);
 }
 int main(void)
 {
-        char n[] = "1234567892434574367823574575678477685785645685876876774586734734563456453743756756784458";
-        char m[] = "9034790663470697234682914569346259634958693246597324659762347956349265983465962349569346";
-        char r[100];
+        //char n[] = "1234567892434574367823574575678477685785645685876876774586734734563456453743756756784458";
+        //char m[] = "9034790663470697234682914569346259634958693246597324659762347956349265983465962349569346";
+        char	n[] = "1934";
+	char	m[] = "299";
+	char r[6];
         char *res;
 
-        res = infinite_add(n, m, r, 100);
-        if (res == 0)
+        res = infinite_add(n, m, r, 2);
+        //printf("%s\n", res = infinite_add(n, m, r, 6));
+        /*if (res == 0)
         {
                 printf("Error\n");
         }
@@ -81,7 +129,6 @@ int main(void)
         {
                 //printf("%s + %s = %s\n", n, m, res);
         }
-		/*
         n = "1234567890";
         m = "1";
         res = infinite_add(n, m, r2, 10);
