571854bbdbb2b974b806896a2db9e87a70630f79
Alfredo Colon <710@holbertonschool.com>
2019-02-23 01:33:58 +0000

    added extra if for -numbers < -9 and changed some ifs to else ifs

--- 0x05-pointers_arrays_strings/100-print_number.c
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -10,14 +10,15 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		print_number(-(n / 10));
+		if (-n < -9)
+			print_number(-(n / 10));
 		print_number(-(n % 10));
 	}
-	if (n > 9)
+	else if (n > 9)
 	{
 		print_number(n / 10);
 		print_number(n % 10);
 	}
-	if (n < 10)
+	else if (n < 10)
 		_putchar(n + '0');
 }
