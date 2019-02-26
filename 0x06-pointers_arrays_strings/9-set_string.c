2b806b43f41170f6fb0c446b1d2755d8f13d25d3
alfredocol <47125664+alfredocol@users.noreply.github.com>
2019-02-25 18:21:52 -0800

    Update 9-set_string.c

--- 0x06-pointers_arrays_strings/9-set_string.c
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -9,13 +9,3 @@ void set_string(char **s, char *to)
 {
 	*s = to;
 }
-int main(void)
-{
-    char *s0 = "Anne Cognet";
-    char *s1 = "Asaia Palacios";
-
-    printf("%s, %s\n", s0, s1);
-    set_string(&s1, s0);
-    printf("%s, %s\n", s0, s1);
-    return (0);
-}
