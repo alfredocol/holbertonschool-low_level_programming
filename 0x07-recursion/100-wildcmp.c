35d35a5c29a84e0458495ab387a8e2dd2a31753d
Alfredo <johndoe@example.com>
2019-02-28 07:05:53 +0000

    have an answer, kinda want to work it out the hard way :)

---
+++ 0x07-recursion/100-wildcmp.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - compares two strings
+ * @s1: string
+ * @s2: string
+ *
+ * Return: 1 if identical, 0 if not
+ */
+int	wildcmp(char *s1, char *s2)
+{
+	(void)s1;
+	(void)s2;
+	return (1);
+}
