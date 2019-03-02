39b9c5212694ab1ae8629c95e2ec6088bd156d16
Alfredo <johndoe@example.com>
2019-03-02 21:38:38 +0000

    betty'd

--- 0x09-argc_argv/3-mul.c
+++ 0x09-argc_argv/3-mul.c
@@ -3,21 +3,21 @@
 #include <stdlib.h>
 
 /**
- * _aredigits - checks if string contains only digits
+ * aredigits - checks if string contains only digits
  * @s: string
  *
  * Return: 0 if non-digit character is found
  */
-int     aredigits(char *s)
+int	aredigits(char *s)
 {
-        while (*s)
-        {
-                if (*s < '0' || *s > '9')
+	while (*s)
+	{
+		if (*s < '0' || *s > '9')
 			if (*s != '-' && *s != '+')
-                        	return (0);
-                s++;
-        }
-        return (1);
+				return (0);
+		s++;
+	}
+	return (1);
 }
 /**
  * _mul - multiplies passed in number strings
@@ -28,30 +28,13 @@ int     aredigits(char *s)
  */
 int     _mul(char *num1, char *num2)
 {
-        int	n1 = atoi(num1);
+	int	n1 = atoi(num1);
 	int	n2 = atoi(num2);
-	
+
 	if (!n1 && !aredigits(num1))
 		return (0);
 	if (!n2 && !aredigits(num2))
-		return (0); 
+		return (0);
 	printf("%d\n", n1 * n2);
-        return (1);
-}
-/**
- * main - calls mul, prints error if applicable
- * @argc: number of arguments
- * @argv: passed in arguments
- *
- * Return: 0
- */
-int     main(int argc, char *argv[])
-{
-        (void)argv;
-        if (argc != 3 || (!_mul(argv[1], argv[2])))
-	{
-		printf("Error\n");
-		return (1);
-	}
-        return (0);
+	return (1);
 }
