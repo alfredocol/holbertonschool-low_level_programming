e5e24f2cdedcc0bd61ede64a415ab26db5e3c810
Alfredo <johndoe@example.com>
2019-03-04 18:32:11 +0000

    working solution

--- 0x07-recursion/100-wildcmp.c
+++ 0x07-recursion/100-wildcmp.c
@@ -7,9 +7,18 @@
  *
  * Return: 1 if identical, 0 if not
  */
-int	wildcmp(char *s1, char *s2)
+int     wildcmp(char *s1, char *s2)
 {
-	(void)s1;
-	(void)s2;
-	return (1);
+	if (!*s1 && !*s2)
+		return (1);
+	if (*s2 == '*')
+	{
+		if (*s1)
+			return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+		if (!*s1)
+			return (wildcmp(s1, s2 + 1));
+	}
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
 }
