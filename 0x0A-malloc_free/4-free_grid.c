ff284487fd39adbfb5ff2a7df8da42902513d069
Alfredo <johndoe@example.com>
2019-03-05 23:18:54 +0000

    working solution

--- 0x0A-malloc_free/4-free_grid.c
+++ 0x0A-malloc_free/4-free_grid.c
@@ -1,1 +1,16 @@
-void free_grid(int **grid, int height);
+#include "holberton.h"
+
+/**
+ * free_grid - frees malloc'd grid memory
+ * @grid: 2D int array
+ * @height: # of subarrays
+ *
+ * Return: void
+ */
+void	free_grid(int **grid, int height)
+{
+	while (height--)
+		free(grid[height]);
+	if (grid)
+		free(grid);
+}
