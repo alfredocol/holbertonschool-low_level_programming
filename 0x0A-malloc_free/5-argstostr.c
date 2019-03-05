de67fb42d79f418a7942dde540826dd212eab139
Alfredo <johndoe@example.com>
2019-03-05 23:43:09 +0000

    working solution

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -1,1 +1,34 @@
-ar *argstostr(int ac, char **av);
+#include "holberton.h"
+
+/**
+ * argstostr - concatenates all args to string
+ * @ac: arg count
+ * @av: 2D array of strings
+ *
+ * Return: concatenated string
+ */
+char *argstostr(int ac, char **av)
+{
+	int	len = ac;
+	int	i, j, k, l;
+	char	*p;
+	char	*temp;
+
+	if (!ac || !av)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+		for (j = 0; av[i][j]; j++)
+			len++;
+	p = (char *)malloc(sizeof(char) * len);
+	if (!p)
+		return (NULL);
+	temp = p;
+	for (k = 0; k < ac; k++)
+	{
+		for (l = 0; av[k][l]; l++)
+			*temp++ = av[k][l];
+		*temp++ = '\n';
+	}
+	*temp = '\0';
+	return (p);
+}
