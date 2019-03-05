e32ac7ae994e887977db4e77816dc38a07356e6c
Alfredo <johndoe@example.com>
2019-03-05 22:56:18 +0000

    working solution

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -1,1 +1,26 @@
-int **alloc_grid(int width, int height);
+#include "holberton.h"
+
+/**
+ * alloc_grid - creates 2D array of ints
+ * @width: # of elements in each sub-array
+ * @height: number of sub-arrays
+ *
+ * Return: 2D int array
+ */
+int	**alloc_grid(int width, int height)
+{
+	int	**grid = (int **)malloc(sizeof((width * height)));
+	int	i, j;
+
+	if (!grid)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(sizeof(width));
+		if (!grid[i])
+			return (NULL);
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+	return (grid);
+}
