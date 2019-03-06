0db32b4663d4af3cc87f6746165398e561eac5da
Alfredo <johndoe@example.com>
2019-03-06 22:37:01 +0000

    faruk'ified to the extreme

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -1,67 +1,31 @@
 #include "holberton.h"
-/**
- * _strlen - returns the length of a string.
- * @s: string
- *
- * Description: returns the length of a string.
- * Return: len of string
- */
-int _strlen(char *s)
-{
-	int count;
 
-	count = 0;
-	while (*s)
-	{
-		count++;
-		s++;
-	}
-	return (count);
-}
 /**
- * _strcpy - copies the string pointed to by src, including
- * the terminating null byte (\0), to the buffer pointed to by dest
- * @src: string to be copied from
- * @dest: string to be copied to
- *
- * Description: copies the string pointed to by src, including
- * the terminating null byte (\0), to the buffer pointed to by dest.
- * Return: pointer to dest
- */
-char	*_strcpy(char *dest, char *src)
-{
-	int i;
-
-	for (i = 0; *(src + i); ++i)
-	{
-		*(dest + i) = *(src + i);
-	}
-	*(dest + i) = '\0';
-	return (dest);
-}
-/**
- * _strdup - duplicates string
+ * _strdup - duplicates a string
  * @str: string
  *
- * Return: pointer to duplicate string
+ * Return: a pointer to the string
  */
-char	*_strdup(char *str)
+char *_strdup(char *str)
 {
-	int	len;
-	char	*dupe;
+	char *s;
+	int i = 0;
 
 	if (str == NULL)
 		return (NULL);
-	len = _strlen(str);
-	if (!*str)
+	while (str[i])
 	{
-		dupe = (char *)malloc(1);
-		dupe[0] = *str;
+		i++;
 	}
-	else
+	s = malloc(sizeof(char) * i + 1);
+	if (s == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i])
 	{
-		dupe = (char *)malloc(len + 1);
-		dupe = _strcpy(dupe, str);
+		s[i] = str[i];
+		i++;
 	}
-	return (dupe);
+	s[i++] = '\0';
+	return (s);
 }
