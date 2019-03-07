84477c7d8c85d684faf32ce37fd6000bf55c38e8
Alfredo <johndoe@example.com>
2019-03-07 00:33:59 +0000

    streamlined, refactored

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -16,62 +16,27 @@ int     _strlen(char *s)
 	return (len);
 }
 /**
- * _strcat - concats src to dest
- * @dest: string
- * @src: string
- *
- * Return: dest
- */
-char *_strcat(char *dest, char *src)
-{
-	int     len = 0;
-	char    *p = dest;
-
-	while (p[len])
-		len++;
-	while (*src)
-		p[len++] = *src++;
-	p[len] = '\0';
-	return (dest);
-}
-/**
- * _strcpy - copies the string pointed to by src, including
- * the terminating null byte (\0), to the buffer pointed to by dest
- * @src: string to be copied from
- * @dest: string to be copied to
- *
- * Description: copies the string pointed to by src, including
- * the terminating null byte (\0), to the buffer pointed to by dest.
- * Return: pointer to dest
- */
-char    *_strcpy(char *dest, char *src)
-{
-	int     i = 0;
-	char    *p = dest;
-
-	while (src[i])
-	{
-		*p++ = src[i];
-		i++;
-	}
-	*p = 0;
-	return (dest);
-}
-/**
  * str_concat - concatentates two strings
  * @s1: string
  * @s2: string
  *
  * Return: pointer to concatenated string
  */
-char *str_concat(char *s1, char *s2)
+char	 *str_concat(char *s1, char *s2)
 {
-	int	len = _strlen(s1) + _strlen(s2);
-	char	*p = !len ? NULL : (char *)malloc(len - 1);
+	int	len, len1, len2, i, j;
+	char	*str;
 
-	if (!p)
+	len1 = !s1 ? 0 : _strlen(s1);
+	len2 = !s2 ? 0 : _strlen(s2);
+	len = len1 +  len2;
+	str = (char *)malloc(len + 1);
+	if (!str)
 		return (NULL);
-	p = _strcpy(p, s1);
-	p = _strcat(p, s2);
-	return (p);
+	for (i = 0; i < len1; i++)
+		str[i] = s1[i];
+	for (j = 0; j < len2; j++, i++)
+		str[i] = s2[j];
+	str[i] = '\0';
+	return (str);
 }
