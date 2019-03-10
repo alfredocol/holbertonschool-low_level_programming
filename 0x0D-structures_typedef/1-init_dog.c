f846e9347aa5794ef453a7cdd26a06f28f70040f
Alfredo Colon <710@holbertonschool.com>
2019-03-10 06:27:36 +0000

    new files

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialized a variable of type struct
+ * @d: struct
+ * @name: str *
+ * @age: float, a very specific age
+ * @owner: str *
+ * 
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d.name = name;
+	*d.age = age;
+	d.owner = owner;
+}
