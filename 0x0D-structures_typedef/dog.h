2cc2f446c15cb0c2a44895362eb5774375c0d706
Alfredo <johndoe@example.com>
2019-03-12 04:35:47 +0000

    ?

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _DOG_H
-#define _DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
  * struct dog - data structure for the canine variety
@@ -23,4 +23,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif
+#endif /* _D0G_H_ */
