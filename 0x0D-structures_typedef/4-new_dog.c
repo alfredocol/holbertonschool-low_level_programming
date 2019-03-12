ee9736a54d6d2b06dddbfdf6f19c237f03415204
alfredocol <47125664+alfredocol@users.noreply.github.com>
2019-03-11 23:43:24 -0700

    Update 4-new_dog.c

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,23 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-/**
- * _strcpy - copies src to dest
- * @dest: string
- * @src: string
- *
- * Return: dest
- */
-char	*_strcpy(char *dest, char *src)
-{
-	char	*p = dest;
-
-	while ((*p++ = *src++))
-		;
-	return (dest);
-}
 /**
  * new_dog - creates new dog
  * @name: string
@@ -28,30 +11,12 @@ char	*_strcpy(char *dest, char *src)
  */
 dog_t	*new_dog(char *name, float age, char *owner)
 {
-	int	len1, len2, i, j;
 	dog_t *d = malloc(sizeof(dog_t));
 
-	/* Return NULL if malloc fails */
 	if (!d)
 		return (NULL);
-	if (name)
-	{
-		for (len1 = 0; name[len1]; len1++)
-			;
-		d->name = malloc(len1 + 1);
-		d->name =  d->name ? _strcpy(d->name, name) : NULL;
-	}
-	else
-		d->name = NULL;
-	if (owner)
-	{
-		for (len2 = 0; name[len2]; len2++)
-			;
-		d->owner = malloc(len2 + 1);
-		d->owner =  d->owner ? _strcpy(d->owner, owner) : NULL;
-	}
-	else
-		d->owner = NULL;
+	d->name = name;
 	d->age = age;
+	d->owner = owner;
 	return (d);
 }
