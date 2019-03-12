8ba6b9e7ebd48b8056b5ae9dfdbf4659fb11bcdb
Alfredo <johndoe@example.com>
2019-03-12 04:58:40 +0000

    solution

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-
+#include <stdio.h>
 /**
  * init_dog - initialized a variable of type struct
  * @d: struct
@@ -10,7 +10,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d.name = name;
-	*d.age = age;
-	d.owner = owner;
+	if (!d)
+		return;
+	d->name = name;
+	d->age = &age;
+	d->owner = owner;
+	printf("%s, %f, %s\n", d->name, d->age, d->owner);
 }
