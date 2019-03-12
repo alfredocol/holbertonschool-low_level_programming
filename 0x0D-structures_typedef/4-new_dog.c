2d0e0d42ce7872ab9af5dad66ee8c8cfe8a41796
Alfredo <johndoe@example.com>
2019-03-12 06:26:51 +0000

    malloc'd out

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,6 +1,23 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strcpy - copies src to dest
+ * @dest: string
+ * @src: string
+ *
+ * Return: dest
+ */
+char	*_strcpy(char *dest, char *src)
+{
+	char	*p = dest;
+
+	while ((*p++ = *src++))
+		;
+	return (dest);
+}
 /**
  * new_dog - creates new dog
  * @name: string
@@ -11,12 +28,30 @@
  */
 dog_t	*new_dog(char *name, float age, char *owner)
 {
+	int	len1, len2, i, j;
 	dog_t *d = malloc(sizeof(dog_t));
 
+	/* Return NULL if malloc fails */
 	if (!d)
 		return (NULL);
-	d->name = name;
+	if (name)
+	{
+		for (len1 = 0; name[len1]; len1++)
+			;
+		d->name = malloc(len1 + 1);
+		d->name =  d->name ? strcpy(d->name, name) : NULL;
+	}
+	else
+		d->name = NULL;
+	if (owner)
+	{
+		for (len2 = 0; name[len2]; len2++)
+			;
+		d->owner = malloc(len2 + 1);
+		d->owner =  d->owner ? strcpy(d->owner, owner) : NULL;
+	}
+	else
+		d->owner = NULL;
 	d->age = age;
-	d->owner = owner;
 	return (d);
 }
