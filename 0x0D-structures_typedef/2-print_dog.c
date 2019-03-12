04d56bd6a12d42c2d638fa0b871bb490c073dbec
Alfredo <johndoe@example.com>
2019-03-12 05:38:23 +0000

    the 80 char limit though...

---
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - prints struct dog
+ * @d: struct
+ *
+ */
+void	print_dog(struct dog *d)
+{
+	/* If d is NULL print nothing. */
+	if (!d)
+		return;
+	/* If an element of d is NULL, print (nil) instead of this element. */
+	/*
+	* too long :..(
+	* printf("%s\n%f\n%s\n", (d->name ? d->name : "nil"),\
+	* d->age, (d->owner ? d->owner : "nil"));
+	*/
+	printf("%s\n", d->name ? d->name : "nil");
+	printf("%f\n", d->age);
+	printf("%s\n", d->owner ? d->owner : "nil");
+}
