ec1709bd15507e1eeb0bf4ec58aa82be1afeb4ed
Alfredo <johndoe@example.com>
2019-03-14 18:07:23 +0000

    draft

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -1,8 +1,29 @@
-#include <stdarg.h>
-#include <stdio.h>
+#include "variadic_functions.h"
 
+/**
+ * print_all - prints variadic arguments
+ * @format: string with types
+ *
+ */
 void	print_all(const char * const format, ...)
 {
 	va_list list;
-
 	
+	va_start(list, format);
+        switch (*format)
+        {
+                case '\0':
+                        break;
+                case 's':
+                        printf("%s\n", va_arg(ap, char *));
+                case 'd':
+                        printf("%d\n", va_arg(ap, int));
+                case 'f':
+                        printf("%f\n", va_arg(ap, double));
+                case 'l':
+                        printf("%lu\n", va_arg(ap, long));
+                default:
+                        format++;
+        }
+        va_end(list);
+}
