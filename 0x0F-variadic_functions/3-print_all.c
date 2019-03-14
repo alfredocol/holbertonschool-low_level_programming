d987ea5c1e9cbf45caa59da65d73956c03eddf94
Alfredo <johndoe@example.com>
2019-03-14 20:12:15 +0000

    working. Tried to avoid a while loop parenting the switch function. I feel like there's a way but...hmm...

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -8,22 +8,30 @@
 void	print_all(const char * const format, ...)
 {
 	va_list list;
-	
+
 	va_start(list, format);
-        switch (*format)
-        {
-                case '\0':
-                        break;
-                case 's':
-                        printf("%s\n", va_arg(ap, char *));
-                case 'd':
-                        printf("%d\n", va_arg(ap, int));
-                case 'f':
-                        printf("%f\n", va_arg(ap, double));
-                case 'l':
-                        printf("%lu\n", va_arg(ap, long));
-                default:
-                        format++;
-        }
-        va_end(list);
+	while (*format)
+	{
+		switch (*format)
+		{
+			case '\0':
+				break;
+			case 's':
+				printf("%s\n", va_arg(ap, char *));
+				break;
+			case 'd':
+				printf("%d\n", va_arg(ap, int));
+				break;
+			case 'f':
+				printf("%f\n", va_arg(ap, double));
+				break;
+			case 'l':
+				printf("%lu\n", va_arg(ap, long));
+				break;
+			default:
+				format++;
+		}
+		format++;
+	}
+	va_end(list);
 }
