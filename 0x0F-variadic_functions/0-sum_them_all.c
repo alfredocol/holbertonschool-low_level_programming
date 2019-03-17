21106444384bb5b8012d8f62238947c13e61a9d7
Alfredo <johndoe@example.com>
2019-03-17 19:25:30 +0000

    updated draft solution to pass betty and provided tests

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -1,14 +1,17 @@
 #include "variadic_funtions.h"
-#include <stdarg.h>
-#include <stdio.h>
-
-int     sum_them_all(const unsigned int n, ...)
+/**
+ * sum_them_all - sums passed in va_args
+ * @n: # of arguments
+ *
+ * Return: sum of passed in ints
+ */
+int	sum_them_all(const unsigned int n, ...)
 {
 	int	sum = 0;
 	va_list	list;
 
 	va_start(list, n);
-	for (sum = 0; va_arg(list, int); sum++)
+	for (sum = 0; n; sum += va_arg(list, int))
 		;
 	va_end(list);
 	return (sum);
