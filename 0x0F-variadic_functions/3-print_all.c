5ae8ca00d2e99f0c4365c477b994e849b6cd8d5c
Alfredo <alfredo@Alfredos-MacBook-Pro.local>
2019-03-22 16:46:46 -0700

    error fixes

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -1,37 +1,43 @@
 #include "variadic_functions.h"
 /**
  * print_all - prints variadic arguments
- * @format: string with types
+ * @format: list of args
  *
  */
 void	print_all(const char * const format, ...)
 {
 	va_list list;
-	char	*s = (char *)format;
-
+	char	*s, *str;
+	
+	if (!format)
+	{
+		printf("\n");
+		return;
+	}
+	s = (char *)format;
 	va_start(list, format);
 	while (*s)
 	{
 		switch (*s)
 		{
-			case '\0':
-				break;
 			case 's':
-				printf("%s\n", va_arg(list, char *));
+				str = va_arg(list, char *);
+				printf("%s", !s ? "(nil)" : str);
 				break;
-			case 'd':
-				printf("%d\n", va_arg(list, int));
+			case 'i':
+				printf("%i", va_arg(list, int));
 				break;
 			case 'f':
-				printf("%f\n", va_arg(list, double));
+				printf("%f", va_arg(list, double));
 				break;
-			case 'l':
-				printf("%lu\n", va_arg(list, long));
+			case 'c':
+				printf("%c", va_arg(list, int));
 				break;
 			default:
-				s++;
+				break;
 		}
 		s++;
 	}
+	printf("\n");
 	va_end(list);
 }
