6689acfdea285722bce15069bb6a8389d5b87d0c
Alfredo <alfredo@Alfredos-MacBook-Pro.local>
2019-03-21 23:29:22 -0700

    draft

---
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "list.h"
+/**
+ * list_len - # of elements in list_t
+ * @h: pointer to head of linked list
+ *
+ * Return - # of elements in list_t
+ */
+size_t	list_len(const list_t *h)
+{
+        list_t  *node;
+        size_t  count = 0;
+
+        if (!h)
+                return (count);
+        /* If str is NULL, print [0] (nil) */
+        for (node = (list_t *)h; node; node = node->next, count++)
+        	;
+	return (count);
+}
