69d6c6f70a390dcfc800aa029321beac893081e0
Alfredo <alfredo@Alfredos-MacBook-Pro.local>
2019-03-21 23:41:18 -0700

    draft

---
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -0,0 +1,38 @@
+#include "list.h"
+
+/**
+ * _strlen - len of string
+ * @s: string
+ *
+ * Return: len of string
+ */
+unsigned int _strlen(char *s)
+{
+	unsigned int len = 0;
+
+	while(s[len])
+		;
+	return (len);
+}
+
+/**
+ * add_node - adds a new node to linked list.
+ * @h: pointer to head of linked list
+ * @str: char * for new node
+ *
+ * Return - the address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+        list_t  *node;
+
+        if (!h)
+                return (NULL);
+	node = malloc(sizeof(list_t));
+	if (!node)
+		return (NULL);
+	node->strdup(str);
+	node->len = (unsigned int)_strlen((char *)str);
+	node->next = *head;
+	return (node);
+}
