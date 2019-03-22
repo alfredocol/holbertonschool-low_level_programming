00dcc6a28c059fdcfbc005d452945d1b832fc257
Alfredo <alfredo@Alfredos-MacBook-Pro.local>
2019-03-21 23:50:49 -0700

    draft

---
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "list.h"
+
+/**
+ * free_list - frees linked list
+ * @h: pointer to head of linked list
+ */
+void	free_list(list_t *head)
+{
+        list_t  *free_me;
+
+        if (!h)
+                return (NULL);
+	while (head)
+	{
+		free_me = head;
+		head = head->next;
+		free_me(free_me->str);
+		free_me(tmp);
+	}
+}
