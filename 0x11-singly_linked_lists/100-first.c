cc6895a65335ec7add72afe120d913478db23b9a
Alfredo <alfredo@Alfredos-MacBook-Pro.local>
2019-03-22 11:33:23 -0700

    added draft for 100-first.c and prototype to list.h

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+/**
+ * startupfun - prints string before main is executed
+ */
+void myStartupFun (void) 
+{ 
+    printf ("You're beat! and yet, you must allow,\nI bore my house upon my back!\n"); 
+}
