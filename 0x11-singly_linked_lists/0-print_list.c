fb6d9f69e160aad29334e6cedf297e66092ea6c8
Alfredo <alfredo@Alfredos-MacBook-Pro.local>
2019-03-21 23:57:21 -0700

    betty'd

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -1,7 +1,7 @@
 #include "list.h"
 
 /**
- * print_list - prints all the elements of a list_t list
+ * print_list - prints all the elements of a list
  * @h: pointer to head node
  *
  * Return: number of nodes
@@ -14,7 +14,13 @@ size_t	print_list(const list_t *h)
 	if (!h)
 		return (count);
 	/* If str is NULL, print [0] (nil) */
-	for (node = (list_t *)h; node; node = node->next, count++)
-		printf("[%u] %s", node->str ? "[0] (nil)\n", node->str : node->len, node->str);
+	for (node = (list_t *)h; node; node = node->next)
+	{
+		if (node->str)
+			printf("[%u] %s\n", node->len, node->str);
+		else
+			printf("[%u] %s\n", "[0] (nil)");
+		count++;
+	}
 	return (count);
 }
