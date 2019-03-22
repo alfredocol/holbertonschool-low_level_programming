0aafca5d3f030f9ab9da8dd059d2769ed150a3df
Alfredo <alfredo@Alfredos-MacBook-Pro.local>
2019-03-22 11:39:59 -0700

    betty'd

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -2,8 +2,8 @@
 /**
  * startupfun - prints string before main is executed
  */
-void startupfun (void)
+void startupfun(void)
 {
-    printf ("You're beat! and yet, you must allow,\n"); 
-    printf ("I bore my house upon my back!\n"); 
+    printf("You're beat! and yet, you must allow,\n");
+    printf("I bore my house upon my back!\n");
 }
