5c8c346b8dd755d1744c0b73e1389569e4a56457
Alfredo <alfredo@Alfredos-MacBook-Pro.local>
2019-03-23 16:31:28 -0700

    prints negative single digit numbers correctly

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -11,6 +11,11 @@ void	print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
+		if (n > -10)
+		{
+			_putchar(-n + '0');
+			return;
+		}
 		print_number(-(n / 10));
 		print_number(-(n % 10));
 	}
