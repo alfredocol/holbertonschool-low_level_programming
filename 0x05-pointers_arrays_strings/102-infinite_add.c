1c5b1c7716adc6915d4d7eb125509c7ee10c6e2a
Alfredo <alfredo@Alfredos-MacBook-Pro.local>
2019-03-23 18:52:34 -0700

    fixed errors

--- 0x05-pointers_arrays_strings/102-infinite_add.c
+++ 0x05-pointers_arrays_strings/102-infinite_add.c
@@ -1,164 +1,98 @@
-#include <unistd.h>
-#include <stdio.h>
-
+#include "holberton.h"
 /**
- * _strlen - returns the length of a string.
+ * _memset - fills memory with a constant byte.
+ * @s: string
+ * @b: character to fill s with
+ * @n: number of bytes to fill
+ *
+ * Return: pointer to s
+ */
+
+void    _memset(char *s, char b, int n)
+{
+	char    *p = s;
+
+	while (n--)
+		*p++ = b;
+}
+/**
+ * _strlen - returns len of string
  * @s: string
  *
- * Description: returns the length of a string.
  * Return: len of string
  */
-int	_strlen(char *s)
+int     _strlen(char *s)
 {
-	int	len = 0;
+	int     len;
 
-	while (s[len])
-		len++;
+	for (len = 0; s[len]; len++)
+		;
 	return (len);
 }
 /**
- * _strlen - returns the length of a string.
+ * rev_numstring - that reverses a string.
  * @s: string
- *
- * Description: returns the length of a string.
- * Return: len of string
+ * @size: len of string
  */
-char	*_bzero(char r[], int size)
+void    rev_numstring(char *s, int size)
 {
-	char	*p = r;
-	
-	while (size--)
-		*p++ = '0';
-	return (r);
-}
-/**
- * _strlen - returns the length of a string.
- * @s: string
- *
- * Description: returns the length of a string.
- * Return: len of string
- */
-char	*add(char r[], int n, int i)
-{
-	int	sum;
-	
-	sum = r[i] - '0' + n;
-	r[i] = sum % 10 + '0';
-	if (sum > 9)
-	{
-		if (!i)
-			return (0);
-		r = add(r, sum / 10, i - 1);
-	}
-	return (r);
-} 
-/**
- * _strlen - returns the length of a string.
- * @s: string
- *
- * Description: returns the length of a string.
- * Return: len of string
- */
-char	*benotzero(char *r, int offset, int len)
-{
-	int	i = 0;
-	int	end = len - offset;
-	printf("%d\n", offset);
-	while (i < end)
-	{
-		r[i] = r[i + offset];
-		i++;
-	}
+	int	len;
+	int	i;
+	char	temp;
 
-	int	j = -1;
-	while (++j < 6)
-		printf("%c", r[j]);
-	printf("\n");
-	return (r);
+	while (--size && s[size] == '0')
+		;
+	len = size;
+	for (i = 0; i < len; i++, len--)
+	{
+		temp = s[i];
+		s[i] = s[len];
+		s[len] = temp;
+		printf("%s\n", s);
+	}
 }
 /**
- * _strlen - returns the length of a string.
- * @s: string
+ * infinite_add - adds two numstrings.
+ * @m: numstring 1
+ * @n: numstring 2
+ * @r: container to sumstring
+ * @size: size of r container
  *
- * Description: returns the length of a string.
- * Return: len of string
+ * Description: that reverses a string.
+ * Return: void
  */
 char *infinite_add(char *n, char *m, char *r, int size)
 {
 	int	nlen = _strlen(n);
 	int	mlen = _strlen(m);
-	int	sum;
-  	int	len = size;
+	int	carryover = 0;
+	int	i, j;
 
-	r = _bzero(r, size);		
-	if (mlen - 1 > size || nlen - 1 > size)
+	/* error check */
+	if (!n || !m || !r || mlen - 1 > size || nlen - 1 > size)
 		return (0);
-	while (size--)
+	/* set all elements to 0 */
+	_memset(r, 0, size);
+	for (i = 0; i < size; i++)
 	{
-		mlen--;
-		nlen--;
-		sum = 0;
-		if (mlen > -1)
-			sum += m[mlen] - '0';
-		if (nlen > -1)
-			sum += n[nlen] - '0';
-		if (!(r = add(r, sum, size)))
-			return (0);
-		if (mlen <= 0 && nlen <= 0)
-			break ;
+		/* update value of arr[i] */
+		if (mlen)
+			r[i] += m[--mlen] - '0';
+		if (nlen)
+			r[i] += n[--nlen] - '0';
+		if (!mlen && !nlen)
+			break;
 	}
-	r = benotzero(r, size, len);
+	/* update carryover */
+	for (j = 0; j <= i; j++)
+	{
+		r[j] += carryover;
+		carryover = r[j] > 9 ? r[j] / 10 : 0;
+		r[j] = r[j] > 9 ? r[j] % 10 : r[j];
+		r[j] += '0';
+	}
+	if (carryover)
+		r[j] = carryover + '0';
+	rev_numstring(r, j + 1);
 	return (r);
-}
-int main(void)
-{
-        //char n[] = "1234567892434574367823574575678477685785645685876876774586734734563456453743756756784458";
-        //char m[] = "9034790663470697234682914569346259634958693246597324659762347956349265983465962349569346";
-        char	n[] = "1934";
-	char	m[] = "299";
-	char r[6];
-        char *res;
-
-        res = infinite_add(n, m, r, 2);
-        //printf("%s\n", res = infinite_add(n, m, r, 6));
-        /*if (res == 0)
-        {
-                printf("Error\n");
-        }
-        else
-        {
-                //printf("%s + %s = %s\n", n, m, res);
-        }
-        n = "1234567890";
-        m = "1";
-        res = infinite_add(n, m, r2, 10);
-        if (res == 0)
-        {
-                printf("Error\n");
-        }
-        else
-        {
-                printf("%s + %s = %s\n", n, m, res);
-        }
-        n = "999999999";
-        m = "1";
-        res = infinite_add(n, m, r2, 10);
-        if (res == 0)
-        {
-                printf("Error\n");
-        }
-        else
-        {
-                printf("%s + %s = %s\n", n, m, res);
-        }
-        res = infinite_add(n, m, r3, 11);
-        if (res == 0)
-        {
-                printf("Error\n");
-        }
-        else
-        {
-                printf("%s + %s = %s\n", n, m, res);
-        }*/
-        return (0);
 }
