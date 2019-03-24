cf248d8fec4f8e1199dfa52b4e46c4ec197ef3fb
Alfredo <alfredo@Alfredos-MacBook-Pro.local>
2019-03-24 16:24:02 -0700

    removed printf statement and update header file

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include <stdio.h>
 /**
  * init_dog - initialized a variable of type struct
  * @d: struct
@@ -15,5 +14,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-	printf("%s, %f, %s\n", d->name, d->age, d->owner);
 }
