88f60e9d648224a77a0c658574539e4fa065d12b
Alfredo <alfredo@Alfredos-MacBook-Pro.local>
2019-03-24 17:12:35 -0700

    refactored some code into strdup

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,20 +1,27 @@
 #include "dog.h"
-#include <stdio.h>
-#include <stdlib.h>
-/**
- * _strcpy - copies src to dest
- * @dest: string
- * @src: string
- *
- * Return: dest
- */
-char	*_strcpy(char *dest, char *src)
-{
-	char	*p = dest;
 
-	while ((*p++ = *src++))
-		;
-	return (dest);
+/**
+ * _strdup - copies src to dest
+ * @s: string to dupe
+ *
+ * Return: pointer to duplicated str
+ */
+char	*_strdup(char *s)
+{
+	char	*d = NULL;
+	int	l = 0, i;
+
+	if (!s)
+		return (NULL);
+	while (s[l])
+		l++;
+	d = malloc(sizeof(char) * l + 1);
+	if (!d)
+		return (NULL);
+	for (i = 0; i < l; i++)
+		d[i] = s[i];
+	d[i + 1] = 0;
+	return (d);
 }
 /**
  * new_dog - creates new dog
@@ -26,26 +33,19 @@ char	*_strcpy(char *dest, char *src)
  */
 dog_t	*new_dog(char *name, float age, char *owner)
 {
-	int	len1, len2;
 	dog_t *d = malloc(sizeof(dog_t));
 
 	if (!d)
 		return (NULL);
 	if (name)
 	{
-		for (len1 = 0; name[len1]; len1++)
-			;
-		d->name = malloc(len1 + 1);
-		d->name =  d->name ? _strcpy(d->name, name) : NULL;
+		d->name = _strdup(name);
 		if (!d->name)
 			return (free(d), NULL);
 	}
 	if (owner)
 	{
-		for (len2 = 0; name[len2]; len2++)
-			;
-		d->owner = malloc(len2 + 1);
-		d->owner =  d->owner ? _strcpy(d->owner, owner) : NULL;
+		d->owner = _strdup(owner);
 		if (!d->owner)
 		{
 			if (d->name)
