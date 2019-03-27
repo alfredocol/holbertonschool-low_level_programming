992a99979aa8d35761d241b116d5d45096dd1b93
Alfredo <alfredo@Alfredos-MacBook-Pro.local>
2019-03-26 22:05:38 -0700

    added parantheses around temp assignment

--- 0x12-more_singly_linked_lists/6-pop_listint.c
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -13,7 +13,7 @@ int	pop_listint(listint_t **head)
 	/* NULL check */
 	if (!head || !*head)
 		return (0);
-	temp = *head->next;
+	temp = (*head)->next;
 	n = (*head)->n;
 	free(*head);
 	*head = temp;
