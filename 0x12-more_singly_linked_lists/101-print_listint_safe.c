48a9b03cb0928b17f231cbfb7bcc7740143da31b
Alfredo <alfredo@Alfredos-MacBook-Pro.local>
2019-03-26 21:12:02 -0700

    removed temp variable and counted nodes

--- 0x12-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x12-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,23 +8,22 @@
 size_t  print_listint_safe(const listint_t *h)
 {
 	size_t		sum = 0;
-	listint_t	*temp;
 
 	/* If the function fails, exit the program with status 98 */
 	if (!h)
 		exit(98);
-	temp = (listint_t *)h;
 	/*
 	 * loop through nodes,
 	 * as long as the address of the current node
 	 * is less than the previous node, print
 	 */
-	while (temp)
+	while (h)
 	{
-		printf("[%p] %d\n", (void *)temp, temp->n);
-		if (!temp->next || (temp->next > temp))
+		printf("[%p] %d\n", (void *)h, h->n);
+		if (!h->next || (h->next > h))
 			break;
-		temp = temp->next;
+		h = h->next;
+		sum++;
 	}
 	/* Returns: the number of nodes in the list */
 	return (sum);
