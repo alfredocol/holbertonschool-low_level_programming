e7e5f8b0f3c91329e7dbaec59e25e2792c75c09f
Alfredo <alfredo@Alfredos-MacBook-Pro.local>
2019-03-26 22:52:47 -0700

    solution

---
+++ 0x12-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * find_listint_loop - find first node in loop
+ * @head: pointer to head node
+ *
+ * Return: the first node in loop or NULL
+ */
+listint_t *find_listint_loop(listint_t *head);
+{
+	while (h)
+	{
+		if (h->next >= h)
+		{
+			printf("Loop starts at [%p] %d\n", (void *)h->next, h->next->n);
+			return (h->next);
+		}
+		h = h->next;
+
+	}
+	return (NULL);
+}
