e2098513954e9d8bf01ab7005d4a40df07bcfccc
Alfredo <alfredo@Alfredos-MacBook-Pro.local>
2019-03-26 21:50:15 -0700

    changed if statement for rpeat node and removed exit check.

--- 0x12-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x12-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,9 +9,6 @@ size_t  print_listint_safe(const listint_t *h)
 {
 	size_t		sum = 0;
 
-	/* If the function fails, exit the program with status 98 */
-	if (!h)
-		exit(98);
 	/*
 	 * loop through nodes,
 	 * as long as the address of the current node
@@ -23,7 +20,7 @@ size_t  print_listint_safe(const listint_t *h)
 		sum++;
 		if (!h->next)
 			break;
-		if (h->next > h)
+		if (h->next >= h)
 		{
 			printf("-> [%p] %d\n", (void *)h->next, h->next->n);
 			break;
