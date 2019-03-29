3a4d003658a590b1bc3008e6ddcc2880a027dd79
Alfredo <alfredo@Alfredos-MacBook-Pro.local>
2019-03-28 17:28:19 -0700

    NULL checked (doy\!) and pseudocoded :)

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -9,9 +9,15 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int	l, sum, n;
 
+	/* NULL check */
+	if (!b)
+		return (0);
+	/* Get length */
 	for (l = 0; b[l]; l++)
+		/* return 0 if char is not '1' or '0' */
 		if (b[l] != '0' && b[l] != '1')
 			return (0);
+	/* Loop through b, add magitudes with 1 to sum */
 	n = 1;
 	sum = 0;
 	while (l--)
