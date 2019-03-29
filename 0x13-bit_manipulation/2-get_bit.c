a5f710aa259b615b260e2204601762fd556dad0d
Alfredo <alfredo@Alfredos-MacBook-Pro.local>
2019-03-28 18:20:54 -0700

    refactored, added overflow check and pseudocoded

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -9,11 +9,11 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int    m = 1;
 
+	/* avoid overflow */
+	if (index > 31)
+		return (-1);
+	/* get to the magnitude/bit for index value */
 	while (index--)
-	{
 		m <<= 1;
-		if (m > n)
-			return (-1);
-	}
 	return ((n & m) ? 1 : 0);
 }
