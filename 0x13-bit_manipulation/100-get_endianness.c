051356d34202a09206b7ba3f1f93df68c5bab3ca
Alfredo <alfredo@Alfredos-MacBook-Pro.local>
2019-03-28 19:51:18 -0700

    solution

---
+++ 0x13-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,9 @@
+#include "holberton.h"
+/**
+ * get_endianness - checks for endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	return ((char)256 ? 1 : 0);
+}
